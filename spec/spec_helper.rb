d66433a8e827c97a07b857fa54aabb7c3d20f75f
Rafael Lima <contato@rafael.adm.br>
2013-09-26 18:26:24 -0300

    Uses environment variables instead of bit_stamp_details.yml on specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,8 +34,8 @@ end
 
 def read_bitstamp_yaml
   Bitstamp.setup do |config|
-    yaml = YAML::load( File.read('bit_stamp_details.yml') )
-    config.key = yaml['bitstamp']['id']
-    config.secret = yaml['bitstamp']['password']
+    raise "You must set environment variable BITSTAMP_KEY and BITSTAMP_SECRET with your username and password to run specs." if ENV['BITSTAMP_KEY'].nil? or ENV['BITSTAMP_SECRET'].nil?
+    config.key = ENV['BITSTAMP_KEY']
+    config.secret = ENV['BITSTAMP_SECRET']
   end
 end
