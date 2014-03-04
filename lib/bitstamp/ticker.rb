0bdfad98555ffdc95716b3972524270007619454
Dominik Masur <dominik.masur@googlemail.com>
2014-03-04 21:54:39 +0100

    Added vwap

--- lib/bitstamp/ticker.rb
+++ lib/bitstamp/ticker.rb
@@ -1,6 +1,6 @@
 module Bitstamp
   class Ticker < Bitstamp::Model
-    attr_accessor :last, :high, :low, :volume, :bid, :ask, :timestamp
+    attr_accessor :last, :high, :low, :volume, :bid, :ask, :timestamp, :vwap
 
     def self.from_api
       Bitstamp::Helper.parse_object!(Bitstamp::Net.get('/ticker').body_str, self)
