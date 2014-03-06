e9e2184c3d00e8150cdfe432d26201130051be64
Sqeaky <toppij@blacktoppstudios.com>
2014-03-05 22:09:51 -0600

    Removed second limit, users should now take care to not exceed 600 requests in 10 minutes on their own. Added .to_s to username

--- lib/bitstamp/net.rb
+++ lib/bitstamp/net.rb
@@ -11,8 +11,8 @@ module Bitstamp
 
       if Bitstamp.configured?
         options[:key] = Bitstamp.key
-        options[:nonce] = Time.now.to_i.to_s
-        options[:signature] = HMAC::SHA256.hexdigest(Bitstamp.secret, options[:nonce]+Bitstamp.client_id+options[:key]).upcase
+        options[:nonce] = (Time.now.to_f*10000).to_i.to_s
+        options[:signature] = HMAC::SHA256.hexdigest(Bitstamp.secret, options[:nonce]+Bitstamp.client_id.to_s+options[:key]).upcase
       end
 
       c.post_body = options.to_query
