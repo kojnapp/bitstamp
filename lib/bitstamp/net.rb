6aaf423c47c817fb5871122d2cbe75981bf6f622
Cuong Vu <cuongvu0103@gmail.com>
2017-08-31 11:20:34 +0700

    Fix base url

--- lib/bitstamp/net.rb
+++ lib/bitstamp/net.rb
@@ -1,7 +1,7 @@
 module Bitstamp
   module Net
     def self.to_uri(path)
-      "https://www.bitstamp.net/api/v2#{path}/"
+      "https://www.bitstamp.net/api#{path}/"
     end
 
     def self.get(path, options={})
